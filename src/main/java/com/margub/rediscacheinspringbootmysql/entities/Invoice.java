adb879ad49bd52a12a102a36662a48157a48baad
mekhan <margub.khan@gmail.com>
2022-06-19 16:43:37 +0530

    Enable redis-caching;

--- src/main/java/com/margub/rediscacheinspringbootmysql/entities/Invoice.java
+++ src/main/java/com/margub/rediscacheinspringbootmysql/entities/Invoice.java
@@ -7,12 +7,13 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.io.Serializable;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-public class Invoice {
+public class Invoice implements Serializable {
 
     private static final long serialVersionUID = -4439114469417994311L;
 
--- src/main/java/com/margub/rediscacheinspringbootmysql/services/InvoiceServiceImpl.java
+++ src/main/java/com/margub/rediscacheinspringbootmysql/services/InvoiceServiceImpl.java
@@ -4,6 +4,9 @@ import com.margub.rediscacheinspringbootmysql.entities.Invoice;
 import com.margub.rediscacheinspringbootmysql.exceptions.InvoiceNotFoundException;
 import com.margub.rediscacheinspringbootmysql.repositories.InvoiceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -21,6 +24,7 @@ public class InvoiceServiceImpl implements InvoiceService {
     }
 
     @Override
+    @CachePut(value="Invoice", key="#invoiceId") // key  value should be same as  updateInvoice second variable name
     public Invoice updateInvoice(Invoice invoice, Integer invoiceId) {
         Invoice foundInvoice = invoiceRepository.findById(invoiceId).orElseThrow(() -> {
             throw new InvoiceNotFoundException(INVOICE_NOT_FOUND);
@@ -31,17 +35,20 @@ public class InvoiceServiceImpl implements InvoiceService {
     }
 
     @Override
+    @CacheEvict(value="Invoice", key="#invoiceId")
     public void deleteInvoice(Integer invoiceId) {
         Invoice invoice = invoiceRepository.findById(invoiceId).orElseThrow(() -> new InvoiceNotFoundException(INVOICE_NOT_FOUND));
         invoiceRepository.delete(invoice);
     }
 
     @Override
+    @Cacheable(value="Invoice", key="#invoiceId")
     public Invoice getOneInvoice(Integer invoiceId) {
         return invoiceRepository.findById(invoiceId).orElseThrow(()-> new InvoiceNotFoundException(INVOICE_NOT_FOUND));
     }
 
     @Override
+    @Cacheable(value="Invoice")
     public List<Invoice> getAllInvoices() {
         return invoiceRepository.findAll();
     }
